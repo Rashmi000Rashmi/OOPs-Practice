dd9d1ef9f86773d3a9ea3a89db730dd837cb8480
Rashmi000Rashmi <ankeykumari000@gmail.com>
2024-08-11 12:55:18 -0400

    Default Constructor practice question

---
+++ Java_Constructor/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+
+        // Cat class
+        Cat cat = new Cat();
+        System.out.println(cat.age);
+        System.out.println(cat.name);
+    }
+}
